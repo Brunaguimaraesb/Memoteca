d5ebcd03bd995f422be8c69ffd27b9723717c585
Brunaguimaraesb <brunaguimaraes.santos@hotmail.com>
2023-02-06 15:27:42 -0300

    Fazendo o projetinho de memoteca

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Pensamento } from './../pensamento';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love Angular',
+    autoria: 'Bruna',
+    modelo: 'modelo3'
+  }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
